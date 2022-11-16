d30381c5ec47a6e068ee678951ba7ae2c2303209
Mikk <mikk@heathmont.net>
2022-11-16 07:50:15 +0200

    uus coetus migra

--- lib/jalka2022/seed.ex
+++ lib/jalka2022/seed.ex
@@ -1,17 +1,5 @@
 defmodule Jalka2022.Seed do
   def seed do
-    # Script for populating the database. You can run it as:
-    #
-    #     mix run priv/repo/seeds.exs
-    #
-    # Inside the script, you can read and write to any of your
-    # repositories directly:
-    # `
-    #     Jalka2022.Repo.insert!(%Jalka2022.SomeSchema{})
-    #
-    # We recommend using the bang functions (`insert!`, `update!`
-    # and so on) as they will fail if something goes wrong.
-
     if Code.ensure_compiled(Jalka2022.Accounts.AllowedUser) &&
          Jalka2022.Accounts.AllowedUser |> Jalka2022.Repo.aggregate(:count, :id) == 0 do
       Enum.each(
@@ -54,6 +42,20 @@ defmodule Jalka2022.Seed do
             away_team_id: Kernel.get_in(attrs, ["awayTeam", "id"]),
             date: Map.get(attrs, "utcDate")
           })
+          |> Jalka2022.Repo.insert!()
+        end
+      )
+    end
+  end
+
+  def seed2 do
+    if Code.ensure_compiled(Jalka2022.Accounts.AllowedUser) &&
+         Jalka2022.Accounts.AllowedUser |> Jalka2022.Repo.aggregate(:count, :id) <= 976 do
+      Enum.each(
+        Jason.decode!(File.read!('/app/lib/jalka2022-0.1.0/priv/repo/data/allowed_users2.json')),
+        fn attrs ->
+          %Jalka2022.Accounts.AllowedUser{}
+          |> Jalka2022.Accounts.AllowedUser.changeset(attrs)
           |> Jalka2022.Repo.insert!()
         end
       )
